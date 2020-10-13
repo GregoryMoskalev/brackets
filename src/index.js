847f03b9214638f54416a16b32609c3823f1564d
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2020-10-13 20:47:52 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2) return false;
+
+  const issue = [];
+
+  for (let char of str) {
+    for (let configElement of bracketsConfig) {
+      if (char === configElement[1] && issue[issue.length - 1] === configElement[0]) {
+        issue.pop();
+      } else if (char === configElement[0]) {
+        issue.push(char);
+      }
+    }
+  }
+
+  return issue.length === 0;
+};
